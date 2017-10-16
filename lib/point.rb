72b01959fb6dd0170eb75a8e11fb09a18f69b703
Coromoto Leon <cleon@ull.es>
2017-10-16 22:26:19 +0100

    clase creada con todas las operaciones

--- lib/point.rb
+++ lib/point.rb
@@ -5,4 +5,16 @@ class Point
     @x, @y = x,y
   end
 
+  def *(value)
+    Point.new(@x * value, @y * value)
+  end
+
+  def -@
+    Point.new(-@x, -@y)
+  end
+
+  def +(other)
+    Point.new(@x + other.x, @y + other.y)
+  end
+
 end
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -13,5 +13,35 @@ describe Point do
       expect(@p1.y).to eq(1)  
     end
   end
-  
+
+  describe "# producto por un escalar" do
+    it "la coordenada x y la coordenada y se multiplican por el valor" do
+      p2 = @p1*5 
+      expect(p2.x).to eq(5)
+      expect(p2.y).to eq(5)
+    end
+    it "Si se multiplica por -1 se obtiene el opuesto" do
+      p2 = @p1*-1 
+      expect(p2.x).to eq(-1)
+      expect(p2.y).to eq(-1)
+    end
+  end
+
+  describe "# opuesto de un punto" do
+    it "la coordenada x y la coordenada y se multiplican por menos uno" do
+      p2 = - @p1 
+      expect(p2.x).to eq(-1)
+      expect(p2.y).to eq(-1)
+    end
+  end
+
+  describe "# suma de dos puntos " do
+    it "coordenada x = original.x + otro.x, coordenada y = original.y + otro.y " do
+      p2 = @p1 + @p1
+      expect(p2.x).to eq(2)
+      expect(p2.y).to eq(2)
+    end
+  end
+
 end
+  
