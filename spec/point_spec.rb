9ecd3253be3fcdeebe7a1caef7e71472ef22c6a2
Coromoto Leon <cleon@ull.es>
2017-10-16 22:14:49 +0100

    Se crea la prueba para comprobar que se crea un punto correctamente

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -1,5 +1,17 @@
 require 'lib/point'
 
 describe Point do
-
+  before :each do
+    @p1 = Point.new(1,1)
+  end    
+    
+  describe "# almacenamiento de las coordenadas" do
+    it "Se almacena correctamente la coordenada X" do
+      @p1.expect(x).to_be eq(1) 
+    end
+    it "Se almacena correctamente la coordenada Y" do
+      @p1.expect(y).to_be eq(1)  
+    end
+  end
+  
 end
